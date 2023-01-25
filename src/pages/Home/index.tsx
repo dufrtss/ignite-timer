81692b97cd4a9daccf0987e076be4bb4737d04f3
Eduardo Freitas <eduardo.freitas@cscezos.com.br>
2023-01-25 16:13:46 -0300

    forms and validations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,7 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import { Play } from 'phosphor-react'
 
 import {
@@ -10,16 +14,40 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Name your project'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Work in</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Name your project"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -35,6 +63,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -48,7 +77,7 @@ export function Home() {
           <span>0</span>
         </ClockContainer>
 
-        <StartClockContainer type="submit" disabled>
+        <StartClockContainer disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Start
         </StartClockContainer>
