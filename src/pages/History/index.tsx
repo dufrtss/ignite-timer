a26c061a96f0bb3a6a6f8e0af402a34ccf9508fb
Eduardo Freitas <eduardo.freitas@cscezos.com.br>
2023-01-25 11:57:45 -0300

    pages and routes :stars:

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,56 @@
+import { HistoryContainer, HistoryTable, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>My history</h1>
+
+      <HistoryTable>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">In progress</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Interrupted</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryTable>
+    </HistoryContainer>
+  )
+}
