831ece0d6a3dddc256b1fd93c0ed8a5c85cc9811
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-02-01 10:44:09 -0300

    context between routes :stars:

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryTable, Status } from './styles'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>My history</h1>
+
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
 
       <HistoryTable>
         <table>
